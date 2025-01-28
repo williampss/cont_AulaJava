e37fdd1d7d50deb9aae3e26ef0bd9edd8d2615b3
williampss <149440991+williampss@users.noreply.github.com>
2025-01-27 21:48:08 -0300

    Exercicios Java
    Testes lógicos

---
+++ exerciciosDeLogicaJava.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+      for(int x = 8; x <= 42; x++){
+        int resto = (x % 2);
+        if (resto == 1){
+          System.out.println(x);
+        }
+      }
+
+    }
+  }
+    
+
+
+
+
+// import java.lang.Math.*;
+// public class Main {
+//     public static void main(String[] args) {
+//         // Math.pow retorna um double, então é necessário fazer o casting para int
+//         int r = (int) Math.pow(3, 3);  // 3 elevado a 3
+//         System.out.println(r);  // Isso vai imprimir 27
+//     }
+// }
+
+
+
+
+
+
+
+
+// for (int cont = 1; cont <= 20; cont = cont + 2){
+      
+    //   System.out.println(cont);
+      
+    // }
+    
+    // int cont = 1;
+    
+    // while(cont<=20){
+      
+    //   System.out.println(cont);
+      
+    //   cont= cont + 2;
+      
+    // }
+
+
+
+
+
+
+
+// System.out.println("digite um valor: ");
+    // int lado1 = 2;
+    
+    // System.out.println("digite um valor: ");
+    // int lado2= 2;
+    
+    // System.out.println("digite um valor: ");
+    // int lado3 = 2;
+    
+    // if(lado1 == lado2  & lado1 != lado3){
+    //   System.out.println("iscaleno");
+    // }
+    
+    // if(lado1 == lado2 &  lado1 == lado3){
+    //     System.out.println("equilatero");
+    // }
+    
+    // if(lado1 != lado2 & lado1 != lado3){
+    //     System.out.println("escaleno");
+    // }
+    
+      
