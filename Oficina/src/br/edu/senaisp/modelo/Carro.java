0aeaafd6692825dcbf3338e4ab06f7987981e1b6
williampss <149440991+williampss@users.noreply.github.com>
2025-02-07 11:54:58 -0300

    Update Carro.java
    Ajuste no conteúdo.

--- Oficina/src/br/edu/senaisp/modelo/Carro.java
+++ Oficina/src/br/edu/senaisp/modelo/Carro.java
@@ -9,23 +9,21 @@ public class Carro {
 	private String  cor;
 	private String placa; 
 	private String nomeCliente;
-	
+
+
+//          permite que a informação seja pega no ambiente teste
 	public String getNome() {
 		return nome;
 		
 	}
 	
+//     permite que a informação editada( ou seja torna possível escrever nesse ambiente)
 	public void setNome(String n) {
 		nome = n;
 	}
 	
 	
-	
-	
-	
-	
-	
-	
+//     construtor
 	public Carro(String nome, String marca, float km, String cor, String placa, String nomeCliente) {
 		super();
 		this.nome = nome;
