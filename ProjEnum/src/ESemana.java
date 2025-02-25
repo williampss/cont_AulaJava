32a17596b73b0410ad6d14269ccd773d67875ee9
williampss <149440991+williampss@users.noreply.github.com>
2025-02-24 21:47:55 -0300

    Conceito de Enum

--- ProjEnum/src/ESemana.java
+++ ProjEnum/src/ESemana.java
@@ -1,11 +1,32 @@
 
+//public enum ESemana {
+//	DOMINGO,
+//	SEGUNDA,
+//	TERCA,
+//	QUARTA,
+//	QUINTA,
+//	SEXTA,
+//	SABADO,
+//
+//}
 public enum ESemana {
-	DOMINGO,
-	SEGUNDA,
-	TERCA,
-	QUARTA,
-	QUINTA,
-	SEXTA,
-	SABADO,
-
+	DOM("Domingo", 1),
+	SEG("Segunda-Feira", 2),	
+	TER("Terça-Feira", 3),
+	QUA("Quarta-Feira", 4),
+	QUI("Quinta-Feira", 5),
+	SEX("Sexta-Feira", 6),
+	SAB("Sábado", 7);
+	
+	
+	
+	
+	public String titulo; 
+	public int indice;
+	ESemana(String s, int i) {
+		this.titulo = s;
+		this.indice = i;
+	}
+	
 }
+
--- ProjEnum/src/Teste.java
+++ ProjEnum/src/Teste.java
@@ -1,23 +1,52 @@
 
+//public class Teste {
+//
+//	public static void main(String[] args) {
+//		
+//		ESemana diaDaSemana = ESemana.SEXTA;
+//		
+//		semana(diaDaSemana.DOMINGO);
+//		
+//		
+//		if (diaDaSemana == ESemana.SEGUNDA || diaDaSemana == ESemana.SABADO) {
+//			System.err.println("Ebaaaa");
+//		}
+//		
+//		
+//
+//	}
+//	
+//	public static void semana (ESemana s) {
+//		
+//	}
+//
+//}
 public class Teste {
 
 	public static void main(String[] args) {
+		ESemana diaDaSemana = 
+				ESemana.valueOf("SAB");
+		System.err.println(ESemana.SEG.titulo);
 		
-		ESemana diaDaSemana = ESemana.SEXTA;
+		System.out.println(ESemana.values()[2]);
 		
-		semana(diaDaSemana.DOMINGO);
-		
-		
-		if (diaDaSemana == ESemana.SEGUNDA || diaDaSemana == ESemana.SABADO) {
-			System.err.println("Ebaaaa");
+		if (diaDaSemana==ESemana.DOM
+				|| diaDaSemana==ESemana.SAB)
+			System.out.println("Ebaaaaa!");
+		else {
+			System.out.println("Remunerado!");
 		}
-		
-		
-
+		for (ESemana s : ESemana.values()) {
+			System.out.println(s.titulo);
+			System.out.println(s.ordinal());
+		}
+	
+		qualquerCoisa(ESemana.SEX);	
 	}
 	
-	public static void semana (ESemana s) {
+    public static void qualquerCoisa(ESemana s) {
 		
 	}
 
 }
+
